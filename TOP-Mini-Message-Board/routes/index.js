2e5a238f3034c57fc21f7a8718656d1ac775984c
Jared Mercer <jaredthebackpacker@gmail.com>
2022-12-31 10:51:46 -0800

    Preparing for deployment

--- TOP-Mini-Message-Board/routes/index.js
+++ TOP-Mini-Message-Board/routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Jared",
+        added: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Amanda",
+        added: new Date(),
+    },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    res.render("index", { title: "Mini Message Board", messages: messages });
+});
+
+router.get("/new", (req, res) => {
+    res.render("form");
+});
+
+router.post("/new", (req, res) => {
+    messages.push({ text: req.body.message, user: req.body.name, added: new Date() });
+    res.redirect("/");
 });
 
 module.exports = router;
